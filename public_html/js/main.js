9831e33fcf7134e711a60f082d24135d8a526c08
Tim <v.timur8484@gmail.com>
2020-10-02 23:36:56 +0500

    Финал верстки

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,4 +2,4 @@
  * @author Timur Valiyev
  * @link https://webprowww.github.io
  */
-"use strict";function _classCallCheck(n,a){if(!(n instanceof a))throw new TypeError("Cannot call a class as a function")}(function(){}).call(void 0);
+"use strict";function _classCallCheck(a,c){if(!(a instanceof c))throw new TypeError("Cannot call a class as a function")}(function(){$("*[data-percent]").each(function(a,c){var n=$(c),t=Number($(this).data("percent"));return n.css({width:"".concat(t,"%")})})}).call(void 0);
--- src/depends/main.depends.js
+++ src/depends/main.depends.js
@@ -1,2 +1,4 @@
 
 //=include vendor/jquery/jquery-3.5.1.min.js
+//=include vendor/fancybox/jquery.fancybox.min.js
+//=include vendor/svg/svg.min.js
