0fd603353b5e69c14885a8b5eb1c2f1579fb3336
Tim <v.timur8484@gmail.com>
2020-10-08 00:41:41 +0500

    Хелпер для работы с поддоменами

---
+++ helpers/Url.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\helpers;
+
+use yii\helpers\Url as YiiHelperUrl;
+use Yii;
+
+/**
+ * Class Url
+ * @package app\helpers
+ */
+class Url extends YiiHelperUrl
+{
+    /**
+     * @param $name string
+     * @return string
+     */
+    public static function toSubdomain($name='')
+    {
+        $name = ($name !== '') ? $name.'.' : $name;
+        $domain = self::rootDomain();
+        $http = Yii::$app->request->isSecureConnection ? 'https://' : 'http://';
+        return $http . $name . $domain;
+    }
+
+    public static function rootDomain()
+    {
+        $domain = Yii::$app->request->hostName;
+        $domainArr = explode('.', $domain);
+        if (count($domainArr) === 3) {
+            $domain = str_replace($domainArr[0].'.', '', $domain);
+        }
+        return $domain;
+    }
+}
