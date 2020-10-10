b3624d920494ad5246f0997de6059a59a621df0f
Tim <v.timur8484@gmail.com>
2020-10-10 18:56:20 +0500

    config web

--- config/web.php
+++ config/web.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    'id' => 'app',
-    'basePath' => dirname(__DIR__),
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
     'components' => [
         'request' => [
             'cookieValidationKey' => 'YQZrWHI8giRkeAmrQ4ZvdIXvczvU_ijT',
