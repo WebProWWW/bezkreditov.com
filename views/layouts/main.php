8b4118a0c1e9993165bcb474c8ce4710ff1193be
Tim <v.timur8484@gmail.com>
2020-10-25 06:47:04 +0500

    Update assets

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,8 +27,8 @@ $description = ArrayHelper::getValue($this->params, 'description', '');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
     <?php $this->registerCsrfMetaTags() ?>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/main.depends.css?v=019">
-    <link rel="stylesheet" href="/css/main.css?v=027">
+    <link rel="stylesheet" href="/css/main.depends.css?v=020">
+    <link rel="stylesheet" href="/css/main.css?v=028">
     <title><?= $this->title ? Html::encode($this->title) : 'Без кредитов' ?></title>
     <meta name="description" content="<?= $description ?>">
     <?php $this->head() ?>
@@ -214,8 +214,8 @@ $description = ArrayHelper::getValue($this->params, 'description', '');
 </div><!-- .d-none -->
 <!-- / МОДАЛЬНЫЕ ОКНА -->
 
-<script src="/js/main.depends.js?v=018"></script>
-<script src="/js/main.js?v=021"></script>
+<script src="/js/main.depends.js?v=019"></script>
+<script src="/js/main.js?v=022"></script>
 
 <?php if (Yii::$app->session->getFlash('is-city', false)): ?>
 <script>if ("function"==typeof window.isCity) { window.isCity() };</script>
