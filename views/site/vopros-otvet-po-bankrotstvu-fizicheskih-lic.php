dff7db15070129b80e84c837ea1c4c167e6d56fe
Tim <v.timur8484@gmail.com>
2020-10-31 12:37:31 +0500

    Update vopros-otvet-po-bankrotstvu-fizicheskih-lic.php

--- views/site/vopros-otvet-po-bankrotstvu-fizicheskih-lic.php
+++ views/site/vopros-otvet-po-bankrotstvu-fizicheskih-lic.php
@@ -166,42 +166,45 @@ $this->params['breadcrumbs'] = [
                 </div><!-- cart wrapper -->
             </div><!-- .col -->
             <div class="col-12 order-xl-3" id="faq-form">
-                <div class="cart">
-                    <div class="cart-body">
-                        <div class="row">
-                            <div class="col-12 col-md-6">
-                                <label class="label">Ваше имя <span class="red">*</span></label>
-                                <input class="input" type="text">
-                            </div><!-- .col -->
-                            <div class="col-12 col-md-6">
-                                <label class="label">Ваш Email <span class="red">*</span></label>
-                                <input class="input" type="text">
-                            </div><!-- .col -->
-                        </div><!-- .row -->
+                <h2>Задать вопрос</h2>
+                <div class="mb-3">
+                    <div class="cart">
+                        <div class="cart-body">
+                            <div class="row">
+                                <div class="col-12 col-md-6">
+                                    <label class="label">Ваше имя <span class="red">*</span></label>
+                                    <input class="input" type="text">
+                                </div><!-- .col -->
+                                <div class="col-12 col-md-6">
+                                    <label class="label">Ваш Email <span class="red">*</span></label>
+                                    <input class="input" type="text">
+                                </div><!-- .col -->
+                            </div><!-- .row -->
 
-                        <label class="label">Ваш вопрос <span class="red">*</span></label>
-                        <textarea class="input" rows="5"></textarea>
+                            <label class="label">Ваш вопрос <span class="red">*</span></label>
+                            <textarea class="input" rows="5"></textarea>
 
-                        <div class="row">
-                            <div class="col-12 col-md order-md-2">
-                                <div class="row align-items-center">
-                                    <div class="col">
-                                        <p class="em-9 right">Прекрипить файл к сообщению</p>
+                            <div class="row">
+                                <div class="col-12 col-md order-md-2">
+                                    <div class="row align-items-center">
+                                        <div class="col">
+                                            <p class="em-9 right">Прекрипить файл к сообщению</p>
+                                        </div>
+                                        <div class="col-auto">
+                                            <span class="btn-sm btn-default">Прекрипить</span>
+                                        </div>
                                     </div>
-                                    <div class="col-auto">
-                                        <span class="btn-sm btn-default">Прекрипить</span>
-                                    </div>
-                                </div>
-                            </div><!-- .col -->
-                            <div class="col-12 col-md-auto order-md-1">
-                                <button class="btn btn-default">Отправить ваш вопрос</button>
-                            </div><!-- .col -->
-                        </div><!-- .row -->
-                    </div>
+                                </div><!-- .col -->
+                                <div class="col-12 col-md-auto order-md-1">
+                                    <button class="btn btn-default">Отправить ваш вопрос</button>
+                                </div><!-- .col -->
+                            </div><!-- .row -->
+                        </div><!-- .cart-body -->
+                    </div><!-- .cart -->
                 </div>
             </div><!-- .col -->
             <div class="col-12 col-lg order-xl-2">
-                <h2 class="h1">Статьи о банкротстве</h2>
+                <h2>Статьи о банкротстве</h2>
                 <div class="row">
                     <div class="col-12 col-lg-6 col-xl-12">
                         <div class="mb-4">
