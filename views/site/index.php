d3d9ae270b919007c9f052303ee72de5310b43bf
Tim <v.timur8484@gmail.com>
2020-11-27 08:09:51 +0500

    Баннеры финал

--- views/site/index.php
+++ views/site/index.php
@@ -20,24 +20,26 @@ $this->params['description'] = 'Бесплатный онлайн тест на 
                 <div class="row align-items-center">
                     <div class="col-12 col-lg order-lg-2 z-2">
                         <div class="px-sm-3 pl-lg-0">
-                            <h2 class="h2 fw-400 my-5">
+                            <h2 class="h2 fw-400 my-4">
                                 <em>
                                     <span class="dialog">Смогу ли я списать свои долги через банкротство?</span>
                                     <span class="dialog">А долги все спишут или частично?</span>
                                 </em>
                             </h2>
-                            <h3 class="h2 center my-5">Хватит гадать пора действовать!</h3>
-                            <p class="em-11 my-5">Пройдите бесплатный онлайн тест и получите предварительное решение о&nbsp;списании ваших долгов* прямо на почту</p>
-                            <div class="row justify-content-center my-5">
-                                <div class="col-auto">
-                                    <a class="btn btn-default" data-fancybox href="#test">Пройти тест</a>
-                                </div><!-- .col -->
-                            </div><!-- .row -->
-                            <p class="my-5">*<small> По 127 Федеральному закону «О банкротстве» можно списать следующие долги: Кредиты и займы. Просрочки по кредитам и кредитными картам. Административные штрафы, долги за коммунальные услуги. Задолженность и пени по налогам.</small></p>
+                            <div class="wall px-sm-5 mb-lg-4">
+                                <h3 class="h2 center my-3">Хватит гадать пора действовать!</h3>
+                                <p class="em-11 my-3">Пройдите бесплатный онлайн тест и получите предварительное решение о&nbsp;списании ваших долгов* и три шикарных <a href="">бонуса</a> от нас прямо на почту</p>
+                                <div class="row justify-content-center my-3">
+                                    <div class="col-auto">
+                                        <a class="btn btn-default" data-fancybox href="#test">Пройти тест</a>
+                                    </div><!-- .col -->
+                                </div><!-- .row -->
+                            </div><!-- .wall -->
+                            <p class="my-4">*<small> По 127 Федеральному закону «О&nbsp;банкротстве» можно списать следующие долги: Кредиты и займы. Просрочки по кредитам. Кредитными картам. Административные штрафы, долги за коммунальные услуги. Задолженность и пени по налогам.</small></p>
                         </div>
                     </div><!-- .col -->
-                    <div class="col-12 col-lg align-self-end order-md-1 z-1">
-                        <img class="img-center" width="480" style="margin-bottom: -3px" src="/img/block-img-1.jpg">
+                    <div class="col-12 col-lg-5 align-self-end order-md-1 z-1">
+                        <img class="img-center" width="277" style="margin-bottom: -3px" src="/img/block-img-5.jpg">
                     </div><!-- .col -->
                 </div><!-- .row -->
             </div>
@@ -730,16 +732,18 @@ $this->params['description'] = 'Бесплатный онлайн тест на 
                 <div class="row align-items-center">
                     <div class="col-12 col-lg-6 order-lg-2 z-2">
                         <div class="px-sm-3">
-                            <h2 class="center my-5">Жители г.&nbsp;<?= $city->name ?> могут бесплатно проверить долги у&nbsp;судебных приставов по фамилии прямо у нас на портале</h2>
-                            <p class="em-11 my-5">Бесплатная проверка долгов у судебных приставов ФССП по фамилии. Укажите Фамилию, Имя, Отчество, Регион и дату рождения физического лица и нажмите проверить долги. В режиме онлайн будут показаны задолженности по кредитам, штрафам ГИБДД, алиментам и другие исполнительные производства должника.</p>
-                            <div class="row my-5">
-                                <div class="col-auto mx-auto">
-                                    <a class="btn btn-default" href="<?= Url::to(['site/index', 'view' => 'uznat-svoi-dolgi']) ?>">Проверить долги</a>
-                                </div><!-- .col -->
-                            </div><!-- .row -->
-                        </div>
+                            <h2 class="center my-4">Жители г.&nbsp;<?= $city->name ?> могут бесплатно проверить долги у&nbsp;судебных приставов по фамилии прямо у нас на портале</h2>
+                            <div class="wall px-sm-5 mb-lg-4">
+                                <p class="em-11 my-4">В режиме онлайн будут показаны задолженности по кредитам, штрафам ГИБДД, алиментам и другие исполнительные производства должника.</p>
+                                <div class="row my-4">
+                                    <div class="col-auto mx-auto">
+                                        <a class="btn btn-default" href="<?= Url::to(['site/index', 'view' => 'uznat-svoi-dolgi']) ?>">Проверить долги</a>
+                                    </div><!-- .col -->
+                                </div><!-- .row -->
+                            </div><!-- .wall -->
+                        </div><!-- .px -->
                     </div><!-- .col -->
-                    <div class="col-12 col-lg align-self-end order-lg-1 z-1">
+                    <div class="col-12 col-lg-6 align-self-end order-lg-1 z-1">
                         <img class="img-center" width="400" style="margin-bottom: -3px" src="/img/block-img-2.jpg">
                     </div><!-- .col -->
                 </div><!-- .row -->
