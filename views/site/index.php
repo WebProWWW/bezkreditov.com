7dd44e0649b24a541bb775c851fc5cae78fa9fe4
Tim <v.timur8484@gmail.com>
2020-12-02 14:53:38 +0500

    блок faq

--- views/site/index.php
+++ views/site/index.php
@@ -811,3 +811,70 @@ $this->params['description'] = 'Бесплатный онлайн тест на 
         </div>
     </div><!-- .container -->
 </section><!-- .section -->
+
+<section class="section bg">
+    <div class="container">
+        <h2 class="h1 center">Вопросы и ответы по банкротству физических лиц</h2>
+        <div class="row justify-content-center mb-3">
+            <div class="col-auto">
+                <a class="tab-ln active">Все</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">Банкротство</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">Коллекторы</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">127 ФЗ</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">Кредит</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">Ипотека</a>
+            </div><!-- .col -->
+            <div class="col-auto">
+                <a class="tab-ln">Имущество</a>
+            </div><!-- .col -->
+        </div><!-- .tab -->
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-6 col-lg-5 col-xl-3">
+                <a class="ihover" href="<?= Url::to(['site/index', 'view' => 'mogut-li-uvolit-za-bankrotstvo-ili-otkazat-v-rabote']) ?>">
+                    <img class="ihover-img" src="/img/faq-th-1.jpg">
+                    <div class="ihover-content">
+                        <h3 class="em-11 fw-600">Могут ли уволить за банкротство или отказать в работе?</h3>
+                        <p class="mt-auto">1 ответ</p>
+                    </div>
+                </a>
+            </div><!-- .col -->
+            <div class="col-12 col-sm-6 col-lg-5 col-xl-3">
+                <a class="ihover" href="<?= Url::to(['site/index', 'view' => 'mogut-li-uvolit-za-bankrotstvo-ili-otkazat-v-rabote']) ?>">
+                    <img class="ihover-img" src="/img/faq-th-2.jpg">
+                    <div class="ihover-content">
+                        <h3 class="em-11 fw-600">Может ли пенсионер или инвалид пройти банкротство, заберут...</h3>
+                        <p class="mt-auto">3 ответа</p>
+                    </div>
+                </a>
+            </div><!-- .col -->
+            <div class="col-12 col-sm-6 col-lg-5 col-xl-3">
+                <a class="ihover" href="<?= Url::to(['site/index', 'view' => 'mogut-li-uvolit-za-bankrotstvo-ili-otkazat-v-rabote']) ?>">
+                    <img class="ihover-img" src="/img/faq-th-3.jpg">
+                    <div class="ihover-content">
+                        <h3 class="em-11 fw-600">Как правильно пройти банкротство?</h3>
+                        <p class="mt-auto">6 ответ</p>
+                    </div>
+                </a>
+            </div><!-- .col -->
+            <div class="col-12 col-sm-6 col-lg-5 col-xl-3">
+                <a class="ihover" href="<?= Url::to(['site/index', 'view' => 'mogut-li-uvolit-za-bankrotstvo-ili-otkazat-v-rabote']) ?>">
+                    <img class="ihover-img" src="/img/faq-th-4.jpg">
+                    <div class="ihover-content">
+                        <h3 class="em-11 fw-600">Если мы с мужем оба должника, можно ли вместе обратиться...</h3>
+                        <p class="mt-auto">2 ответа</p>
+                    </div>
+                </a>
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+</section><!-- .section -->
