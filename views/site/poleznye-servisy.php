01c6e88b333eef6bd03c47269ce93569a2b06490
Tim <v.timur8484@gmail.com>
2020-12-07 23:10:23 +0500

    svg icons

--- views/site/poleznye-servisy.php
+++ views/site/poleznye-servisy.php
@@ -35,14 +35,14 @@ $this->params['breadcrumbs'] = [
     <div class="container">
         <h2 class="center">Онлайн-сервисы портала «Без кредитов»</h2>
         <div class="row">
-            <div class="mb-4 col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-2.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/report_analysis__monochromatic.svg">
                             </div><!-- .col -->
-                            <div class="col-12">
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Онлайн подбор юриста по банкротству</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -54,14 +54,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-8 col-lg-3">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-md-6 col-lg-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-3.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/calendar.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-md-6 col-lg-12">
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Кредитный калькулятор</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -73,14 +73,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-8 col-lg-3">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-md-6 col-lg-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-4.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/calculator.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-md-6 col-lg-12">
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Калькулятор рефинансирования</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -92,14 +92,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-5.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/credit_card_monochromatic.svg">
                             </div><!-- .col -->
-                            <div class="col-12">
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Ипотечный калькулятор</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -111,13 +111,12 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-
             <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
                             <div class="col-12 col-sm-6 col-md-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-6.jpg">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/accountant.svg">
                             </div><!-- .col -->
                             <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Онлайн калькулятор по банкротству</p>
@@ -131,14 +130,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-4 col-lg-6">
+            <div class="mb-4 col-12 col-md-4 col-lg-6">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-lg-6">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-7.jpg">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/consulting.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
                                 <p class="center">Онлайн консультация по банкротству</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -150,14 +149,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-8.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/quality_check.svg">
                             </div><!-- .col -->
-                            <div class="col-12">
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Проверка долгов онлайн</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -169,15 +168,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-
-            <div class="mb-4 col-12 col-sm-6 col-md-4 col-lg-6">
+            <div class="mb-4 col-12 col-md-4 col-lg-6">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-lg-6">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-9.jpg">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/checklist_.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
                                 <p class="center">Онлайн тест на списание долгов, кредитов</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -189,14 +187,14 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-sm-6 col-md-8 col-lg-6">
+            <div class="mb-4 col-12 col-md-4 col-lg-6">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-md-6">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-10.jpg">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/wallet_monochromatic.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-md-6">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
                                 <p class="center">Проверка и оплата штрафов ГИБДД</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -208,55 +206,52 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-            <div class="mb-4 col-12 col-md-6">
-                <div>
-                    <div class="cart mb-4">
-                        <div class="cart-body">
-                            <div class="row align-items-center my-auto">
-                                <div class="col-12 col-sm-6">
-                                    <img class="img-center" width="300" height="300" src="/img/i-img-11.jpg">
-                                </div><!-- .col -->
-                                <div class="col-12 col-sm-6">
-                                    <p class="center">Налоги: проверка и оплата онлайн</p>
-                                    <div class="row">
-                                        <div class="col-auto mx-auto">
-                                            <a class="btn-sm btn-default" href="">Попробовать</a>
-                                        </div>
-                                    </div>
-                                </div><!-- .col -->
-                            </div><!-- .row -->
-                        </div><!-- .cart-body -->
-                    </div><!-- .cart -->
-                </div>
-
-                <div>
-                    <div class="cart">
-                        <div class="cart-body">
-                            <div class="row align-items-center my-auto">
-                                <div class="col-12 col-sm-6">
-                                    <img class="img-center" width="300" height="300" src="/img/i-img-12.jpg">
-                                </div><!-- .col -->
-                                <div class="col-12 col-sm-6">
-                                    <p class="center">Калькулятор налога с продажи квартиры</p>
-                                    <div class="row">
-                                        <div class="col-auto mx-auto">
-                                            <a class="btn-sm btn-default" href="">Попробовать</a>
-                                        </div>
-                                    </div>
-                                </div><!-- .col -->
-                            </div><!-- .row -->
-                        </div><!-- .cart-body -->
-                    </div><!-- .cart -->
-                </div>
-            </div><!-- .col -->
-            <div class="mb-4 col-12 col-md-6">
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
                 <div class="cart">
                     <div class="cart-body">
                         <div class="row align-items-center my-auto">
-                            <div class="col-12 col-sm-6">
-                                <img class="img-center" width="300" height="300" src="/img/i-img-13.jpg">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/transaction__monochromatic.svg">
                             </div><!-- .col -->
-                            <div class="col-12 col-sm-6">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <p class="center">Налоги: проверка и оплата онлайн</p>
+                                <div class="row">
+                                    <div class="col-auto mx-auto">
+                                        <a class="btn-sm btn-default" href="">Попробовать</a>
+                                    </div>
+                                </div>
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .cart-body -->
+                </div><!-- .cart -->
+            </div><!-- .col -->
+            <div class="mb-4 col-12 col-md-4 col-lg-6">
+                <div class="cart">
+                    <div class="cart-body">
+                        <div class="row align-items-center my-auto">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/coins_monochromatic.svg">
+                            </div><!-- .col -->
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6">
+                                <p class="center">Калькулятор налога с продажи квартиры</p>
+                                <div class="row">
+                                    <div class="col-auto mx-auto">
+                                        <a class="btn-sm btn-default" href="">Попробовать</a>
+                                    </div>
+                                </div>
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .cart-body -->
+                </div><!-- .cart -->
+            </div><!-- .col -->
+            <div class="mb-4 col-12 col-md-4 col-lg-3">
+                <div class="cart">
+                    <div class="cart-body">
+                        <div class="row align-items-center my-auto">
+                            <div class="col-12 col-sm-6 col-md-12">
+                                <img class="img-center" width="200" height="200" src="/img/icon-svg/revenue__monochromatic.svg">
+                            </div><!-- .col -->
+                            <div class="col-12 col-sm-6 col-md-12">
                                 <p class="center">Калькулятор пеней и штрафов</p>
                                 <div class="row">
                                     <div class="col-auto mx-auto">
@@ -268,7 +263,6 @@ $this->params['breadcrumbs'] = [
                     </div><!-- .cart-body -->
                 </div><!-- .cart -->
             </div><!-- .col -->
-
         </div><!-- .row -->
     </div><!-- .container -->
 </section><!-- .section -->
