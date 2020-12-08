d6660c0df3887a94e044f2d759ffd134b537b42d
Tim <v.timur8484@gmail.com>
2020-12-08 05:06:11 +0500

    Проверить запрет на выезд из РФ

--- views/site/index.php
+++ views/site/index.php
@@ -545,6 +545,29 @@ $this->params['description'] = 'Бесплатный онлайн тест на 
     </div><!-- .container -->
 </section><!-- .section -->
 
+
+<section class="section" style="background: #97C7D1; overflow: hidden;" id="zapret_viezda_iz_rf">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 align-self-center">
+                <h2>Онлайн сервис: Проверка запрета на выезд из РФ</h2>
+                <h3 class="fw-400">Как проверить запрет на выезд за границу:</h3>
+                <p>На сайте “Без кредитов” вы можете проверить запрет на выезд из страны. Для проверки необходимо указать фамилию, имя, дату рождения, паспортные данные.</p>
+                <p>База сайта обновляются каждый день и берет данные из базы ФССП, ФНС, ФМС, ФТС, МВД.</p>
+                <div class="row">
+                    <div class="col-auto">
+                        <a class="btn-default" href="<?= Url::to(['site/index', 'view' => 'zapret_viezda_iz_rf']) ?>">Проверить</a>
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .col -->
+            <div class="col-12 col-md-6 align-self-end">
+                <img class="img-center" src="/img/img-3.jpg" style="margin-bottom: -13px">
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+</section><!-- .section -->
+
+
 <section class="section bg">
     <div class="container">
 
